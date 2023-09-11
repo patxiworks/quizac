96fe8660b4d8b80d6b847ba2081de3cde8370d8f
Bimpe1 <adebimpe.adetoba@pau.edu.ng>
2023-09-11 12:13:57 +0100

    Implemented user history data-#49

--- components/dashboard/history-summary.js
+++ components/dashboard/history-summary.js
@@ -1,12 +1,21 @@
-import { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Image from 'next/image';
 import HistoryInfo from "./history";
 import PortalPopup from "./portal-popup";
+import { collection, getDocs, query, where, orderBy } from 'firebase/firestore';
+import { db, app } from "@/firebase";
+import { getAuth } from "firebase/auth";
+
 import styles from "./styles/history-summary.module.css";
 
+const auth = getAuth(app);
+
 const HistorySummary = () => {
+  const [currentLevel, setCurrentLevel] = useState(1);
   const [isHistoryPopupOpen, setHistoryPopupOpen] = useState(false);
   const mainContainer = "dashboard";
+  const [userHistory, setUserHistory] = useState([]);
+  const [latestTimestamp, setLatestTimestamp] = useState(null);
 
   const openHistoryPopup = useCallback(() => {
     setHistoryPopupOpen(true);
@@ -20,75 +29,90 @@ const HistorySummary = () => {
     mainDiv.style.position = "relative";
   }, []);
 
+  useEffect(() => {
+    async function fetchUserHistory() {
+      if (auth?.currentUser) {
+        const userEmail = auth.currentUser.email;
+
+        const quizzResultCollection = collection(
+          db,
+          "quizzresult",
+          userEmail,
+          `level${currentLevel}`
+        );
+
+        const queryObj = query(
+          quizzResultCollection,
+          where('userEmail', '==', auth.currentUser.email),
+          where('category', '==', 'art'),
+          orderBy('timestamp', 'desc'), 
+        );
+
+        try {
+          const querySnapshot = await getDocs(queryObj);
+
+          let totalScore = 0;
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            totalScore += data.score || 0;
+          });
+
+          setLatestTimestamp(querySnapshot.docs[0]?.data().timestamp || null);
+
+          const totalScoreEntry = {
+            id: 'art',
+            timestamp: latestTimestamp,
+            category: 'Art',
+            score: totalScore,
+            title: 'Total Score',
+          };
+
+          setUserHistory([totalScoreEntry]);
+        } catch (error) {
+          console.error('Error fetching user history:', error);
+        }
+      }
+    }
+
+    fetchUserHistory();
+  }, [currentLevel,latestTimestamp]);
+
+  function formatTimestamp(timestamp) {
+    if (timestamp) {
+      const date = timestamp.toDate();
+      return date.toLocaleString(); 
+    }
+    return 'N/A';
+  }
+
   return (
     <>
       <div className={styles.boxContainer}>
         <div className={styles.titleBox}>
-            <div className={styles.title}>
-              <div className={styles.yourHistory}>Your History</div>
-            </div>
+          <div className={styles.title}>
+            <div className={styles.yourHistory}>Your History</div>
           </div>
+        </div>
         <div className={styles.infoBox}>
-          <div className={styles.historyRow}>
-            <div className={styles.historyCellDate}>
-              <div className={styles.historyDate}>Jul 20, 10:30</div>
+          {userHistory.map((historyItem) => (
+            <div key={historyItem.id} className={styles.historyRow}>
+              <div className={styles.historyCellDate}>
+                <div className={styles.historyDate}>
+                  {formatTimestamp(historyItem.timestamp)}
+                </div>
+              </div>
+              <div className={styles.historyCellCategory}>
+                <div className={styles.historyDate}>{historyItem.category}</div>
+              </div>
+              <div className={styles.historyCellPoints}>
+                <div className={styles.historyDate}>{historyItem.score} pt</div>
+              </div>
+              <div className={styles.historyCellBadge}>
+                <div className={styles.historyDate}>Gold</div>
+                <div className={styles.historyBadgeIcon} />
+              </div>
             </div>
-            <div className={styles.historyCellCategory}>
-              <div className={styles.historyDate}>Art</div>
-            </div>
-            <div className={styles.historyCellPoints}>
-              <div className={styles.historyDate}>1000 pt</div>
-            </div>
-            <div className={styles.historyCellBadge}>
-              <div className={styles.historyDate}>Gold</div>
-              <div className={styles.historyBadgeIcon} />
-            </div>
-          </div>
-          <div className={styles.historyRow}>
-            <div className={styles.historyCellDate}>
-              <div className={styles.historyDate}>Jul 20, 10:30</div>
-            </div>
-            <div className={styles.historyCellCategory}>
-              <div className={styles.historyDate}>Art</div>
-            </div>
-            <div className={styles.historyCellPoints}>
-              <div className={styles.historyDate}>1000 pt</div>
-            </div>
-            <div className={styles.historyCellBadge}>
-              <div className={styles.historyDate}>Gold</div>
-              <div className={styles.historyBadgeIcon} />
-            </div>
-          </div>
-          <div className={styles.historyRow}>
-            <div className={styles.historyCellDate}>
-              <div className={styles.historyDate}>Jul 20, 10:30</div>
-            </div>
-            <div className={styles.historyCellCategory}>
-              <div className={styles.historyDate}>Art</div>
-            </div>
-            <div className={styles.historyCellPoints}>
-              <div className={styles.historyDate}>1000 pt</div>
-            </div>
-            <div className={styles.historyCellBadge}>
-              <div className={styles.historyDate}>Gold</div>
-              <div className={styles.historyBadgeIcon} />
-            </div>
-          </div>
-          <div className={styles.historyRow}>
-            <div className={styles.historyCellDate}>
-              <div className={styles.historyDate}>Jul 20, 10:30</div>
-            </div>
-            <div className={styles.historyCellCategory}>
-              <div className={styles.historyDate}>Art</div>
-            </div>
-            <div className={styles.historyCellPoints}>
-              <div className={styles.historyDate}>1000 pt</div>
-            </div>
-            <div className={styles.historyCellBadge}>
-              <div className={styles.historyDate}>Gold</div>
-              <div className={styles.historyBadgeIcon} />
-            </div>
-          </div>
+          ))}
           <div className={styles.moreLink}>
             <a className={styles.moreText} onClick={openHistoryPopup}>
               See more
