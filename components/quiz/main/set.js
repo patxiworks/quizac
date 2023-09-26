03a93aafd4c0b92bea01ae661db1a6d0c98259f6
adaobi15 <93054208+adaobi15@users.noreply.github.com>
2023-09-27 00:04:26 +0100

    correction of results logic

--- components/quiz/main/set.js
+++ components/quiz/main/set.js
@@ -1,29 +1,44 @@
 import { db } from '../../../firebase';
-import { doc, setDoc, collection, updateDoc, getDoc } from 'firebase/firestore'; 
+import { doc, setDoc, collection, updateDoc, getDoc, addDoc, increment } from 'firebase/firestore';
 
 const setUserScoreWithLevel = async (userId, category, title, level, score) => {
   try {
-    const quizId = `${category}_${title}`;
     const userDocRef = doc(db, 'results', userId);
     const scoresCollectionRef = collection(userDocRef, 'scores');
-    const scoreDocRef = doc(scoresCollectionRef, quizId);
+    const categoryDocRef = doc(scoresCollectionRef, category);
+    const categoryDocSnapshot = await getDoc(categoryDocRef);
 
-    
-    const scoreDocSnapshot = await getDoc(scoreDocRef);
-    
-    if (scoreDocSnapshot.exists()) {
-      const scoreDocData = {
-        [level]: score,
-      };
+    if (!categoryDocSnapshot.exists()) {
+      await setDoc(categoryDocRef, {});
+    }
 
-      
-      await updateDoc(scoreDocRef, scoreDocData);
+    const titleSubcollectionRef = collection(categoryDocRef, title);
+    const levelCounterDocRef = doc(titleSubcollectionRef, 'levelCounter');
+    const levelCounterDocSnapshot = await getDoc(levelCounterDocRef);
+    let nextLevelNumber = 1;
+
+    if (levelCounterDocSnapshot.exists()) {
+      nextLevelNumber = levelCounterDocSnapshot.data().nextNumber;
+      await updateDoc(levelCounterDocRef, {
+        nextNumber: increment(1),
+      });
     } else {
-      await setDoc(scoreDocRef, {
-        [level]: score,
+      await setDoc(levelCounterDocRef, {
+        nextNumber: nextLevelNumber + 1,
       });
     }
-  //console.log(`Score for user ${userId} in level ${level} of ${quizId} set to ${score}`);
+    const levelDocRef = doc(titleSubcollectionRef, `${level}_${nextLevelNumber}`);
+    const levelDocSnapshot = await getDoc(levelDocRef);
+
+    if (levelDocSnapshot.exists()) {
+      await updateDoc(levelDocRef, {
+        score: score,
+      });
+    } else {
+      await setDoc(levelDocRef, {
+        score: score,
+      });
+    }
   } catch (error) {
     console.error('Error setting user score:', error);
   }
