36a618e3bb0d06cc6d47d29b39d6d75324a7ceed
Patrick Enaholo <patxiworks@gmail.com>
2024-04-16 17:39:56 +0100

    added aggregate score

--- mapapp/maps/data.js
+++ mapapp/maps/data.js
@@ -1,34 +1,3 @@
-/*const setUserScoreWithLevel = async (userId, category, title, level, score, params=null) => {
-    try {
-      const categoryDocRef = doc(db, 'categories', category);
-      //const quizScoresDocRef = doc(categoryDocRef, 'titles', title, 'scores', userId);
-      //const quizScoresDocSnapshot = await getDoc(quizScoresDocRef);
-      const usersCollectionRef = collection(categoryDocRef, 'users');
-      const userDocRef = doc(usersCollectionRef, userId);
-      const scoresCollectionRef = collection(userDocRef, 'scores');
-      const quizTitleDocRef = doc(scoresCollectionRef, title);
-      const quizTitleDocSnapshot = await getDoc(quizTitleDocRef);
-  
-      if (quizTitleDocSnapshot.exists()) {
-        await updateDoc(quizTitleDocRef, {
-          //[`${level}.score`]: score,
-          //[`${level}.timestamp`]: Date.now(),
-          [level]: arrayUnion({ score, timestamp: Date.now(), params }),
-        });
-      } else {
-        await setDoc(quizTitleDocRef, {
-          [level]: [{
-            score,
-            timestamp: Date.now(),
-            params,
-          }],
-        });
-      }
-    } catch (error) {
-      console.error('Error setting user score:', error);
-    }
-  };*/
-
 
 const saveToLocalStorage = (title, level, score, params=null) => {
     const storage = "quizac"
